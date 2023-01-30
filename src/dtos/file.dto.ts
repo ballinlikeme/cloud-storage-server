526d7f700be4d600f57bd204307ac3425370dd96
ballinlikeme <maxim.spirkin2003@gmail.com>
2023-01-30 23:28:01 +0300

    feat: dtos

---
+++ src/dtos/file.dto.ts
@@ -0,0 +1,6 @@
+export class FileDto {
+    readonly userId: number;
+    readonly parentId?: number;
+    readonly name: string;
+    readonly type: string;
+}
---
+++ src/dtos/user.dto.ts
@@ -0,0 +1,14 @@
+import {IsEmail, IsString, Length} from "class-validator";
+
+export class UserDto {
+    @IsString({message: "Поле должно иметь тип string"})
+    @IsEmail({}, {message: "Некорректный email"})
+    readonly email: string;
+    @IsString({message: "Поле должно иметь тип string"})
+    @Length(5, 16, {message: "Минимальная длина пароля 5 символов, максимальная 16 символов"})
+    readonly password: string;
+    @IsString({message: "Поле должно иметь тип string"})
+    @Length(2,undefined, {message: "Минимальная длина имени 2 символа"})
+    readonly name: string;
+    readonly imageUrl?: string;
+}
