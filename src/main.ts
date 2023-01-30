c59966fd920ffbc4f14f3d89865db3e3f2fb13c3
ballinlikeme <maxim.spirkin2003@gmail.com>
2023-01-30 23:27:26 +0300

    feat: main configuration

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import {NestFactory} from "@nestjs/core";
+import {ApplicationModule} from "./application.module";
+import {ValidationPipe} from "@nestjs/common";
+
+const start = async () => {
+
+    const PORT = process.env.PORT || 7000
+
+    const app = await NestFactory.create(ApplicationModule, {cors: true})
+    app.useGlobalPipes(new ValidationPipe({
+        forbidUnknownValues: false
+    }))
+    await app.listen(5000, () => console.log('Server started on port ' + PORT))
+}
+
+start()
